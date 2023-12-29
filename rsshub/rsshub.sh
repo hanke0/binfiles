3cadc1f5d0af57d7b8efe10591498e667dcc7c4e
hanke <hanke0@outlook.com>
2023-12-29 04:05:48 +0000

    update rsshub

--- rsshub/rsshub.sh
+++ rsshub/rsshub.sh
@@ -10,3 +10,7 @@ mkdir -p ./tmp
 rm -rf ./tmp/app
 docker cp "$id:/app" ./tmp/
 docker rm -v "$id"
+mv ./tmp/app ./tmp/rsshub
+cd ./tmp
+tar czf ../rsshub.$(date +%Y-%m-%d).tar.gz ./rsshub
+rm -rf ./tmp
