b09f4fce278465e60bd12aef4c37e2cab99ea74e
hanke0 <hanke0@outlook.com>
2024-01-03 22:29:10 +0800

    update rsshub

--- rsshub/rsshub.sh
+++ rsshub/rsshub.sh
@@ -6,11 +6,20 @@ cd "$(dirname "$0")"
 pwd
 docker pull diygod/rsshub:latest
 id=$(docker create diygod/rsshub:latest)
+rm -rf ./tmp
 mkdir -p ./tmp
 rm -rf ./tmp/app
 docker cp "$id:/app" ./tmp/
 docker rm -v "$id"
 mv ./tmp/app ./tmp/rsshub
 cd ./tmp
-tar czf ../rsshub.$(date +%Y-%m-%d).tar.gz ./rsshub
+date +%Y-%m-%d >./rsshub/version.txt
+cat >./rsshub/run.sh <<EOF
+#!/bin/bash
+
+yarn start
+EOF
+
+tar czf ../rsshub.tar.gz ./rsshub
+cd ..
 rm -rf ./tmp
