7eb3bb73d40de99be7c1006b69c0a10b7859fdc2
hanke <hanke0@outlook.com>
2024-02-07 13:06:52 +0800

    csp-report parse report-to style body

--- csp-report/csp-report.go
+++ csp-report/csp-report.go
@@ -18,20 +18,6 @@ import (
 //go:embed index.html
 var indexHTML []byte
 
-type JSONData struct {
-	CSPReport `json:"csp-report"`
-}
-
-// CSPReport 是 CSP 报告的结构体，你可以根据需要增减字段
-type CSPReport struct {
-	BlockedURI         string `json:"blocked-uri"`
-	DocumentURI        string `json:"document-uri"`
-	EffectiveDirective string `json:"effective-directive"`
-	OriginalPolicy     string `json:"original-policy"`
-	Referrer           string `json:"referrer"`
-	ViolatedDirective  string `json:"violated-directive"`
-}
-
 type RotateWriter struct {
 	lock     sync.Mutex
 	filename string
@@ -105,14 +91,6 @@ func (w *RotateWriter) rotate() (err error) {
 	// Create a file.
 	w.fp, err = os.Create(w.filename)
 	return
-}
-
-func logit(data *CSPReport) {
-	enc := json.NewEncoder(cspWriter)
-	enc.SetEscapeHTML(false)
-	if err := enc.Encode(data); err != nil {
-		log.Printf("write log error: %s", err)
-	}
 }
 
 type loggingResponseWriter struct {
@@ -192,27 +170,69 @@ func authSuccess(token string) bool {
 
 var cspWriter *RotateWriter
 
-func cspReport(w http.ResponseWriter, r *http.Request) {
-	var data JSONData
-	if debugMode {
-		b, err := io.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-		log.Println(string(b))
-		if err := json.Unmarshal(b, &data); err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-	} else {
-		var dec = json.NewDecoder(r.Body)
-		if err := dec.Decode(&data); err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
+func logit(data []CSPReport) {
+	for _, v := range data {
+		if v.BlockedURI != "" {
+			enc := json.NewEncoder(cspWriter)
+			enc.SetEscapeHTML(false)
+			if err := enc.Encode(data); err != nil {
+				log.Printf("write log error: %s", err)
+			}
 		}
 	}
-	go logit(&data.CSPReport)
+}
+
+type reportURIBody struct {
+	CSPReport `json:"csp-report"`
+}
+
+type reportTOBody []reportTOData
+
+type reportTOData struct {
+	Body CSPReport `json:"csp-report"`
+}
+
+// CSPReport 是 CSP 报告的结构体，你可以根据需要增减字段
+type CSPReport struct {
+	BlockedURI         string `json:"blocked-uri" json:"blockedURL"`
+	DocumentURI        string `json:"document-uri" json:"documentURL"`
+	EffectiveDirective string `json:"effective-directive" json:"effectiveDirective"`
+	OriginalPolicy     string `json:"original-policy" json:"originalPolicy"`
+	Referrer           string `json:"referrer" json:"referrer"`
+	Disposition        string `json:"disposition"`
+}
+
+func unmarshalBody(b []byte) ([]CSPReport, error) {
+	var a reportURIBody
+	if err := json.Unmarshal(b, &a); err == nil {
+		return []CSPReport{a.CSPReport}, nil
+	}
+	var c reportTOBody
+	if err := json.Unmarshal(b, &a); err != nil {
+		return nil, err
+	}
+	var reports []CSPReport
+	for _, v := range c {
+		reports = append(reports, v.Body)
+	}
+	return reports, nil
+}
+
+func cspReport(w http.ResponseWriter, r *http.Request) {
+	b, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if debugMode {
+		log.Printf("Content-Type=%s, Body=%s", r.Header.Get("Content-Type"), string(b))
+	}
+	reports, err := unmarshalBody(b)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	go logit(reports)
 	w.WriteHeader(http.StatusOK)
 }
 
